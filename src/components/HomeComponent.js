705dcc914102fa9fb61128d66bd90e575cae1f7b
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-06-17 20:59:41 +0530

    config.

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -19,7 +19,7 @@ function Home() {
             </div>
             <div className="row row-content">
                     <div className="col-12 col-md-3 mt-5 ">
-                        <img src="assets/images/team.png" alt="team"/>
+                        <img src='assets/images/team.png' alt="team"/>
                     </div>
                 <div className="col-12 col-md-8 mt-5 ">
                     <h3>Our Team</h3>
