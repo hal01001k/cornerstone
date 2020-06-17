f86a3f4c276f0de2e34412a8a9bc8e4e16489d07
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-06-17 20:49:08 +0530

    Updates

---
+++ precache-manifest.3a239d15442e4963b4c1cfaa2e2e0169.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc41b1164d3e86c8889e189c5c3e9daf",
+    "url": "/cornerstone/index.html"
+  },
+  {
+    "revision": "6878b74c1bdf65f15c53",
+    "url": "/cornerstone/static/css/2.3abd9552.chunk.css"
+  },
+  {
+    "revision": "556d9d25f6d803f7d899",
+    "url": "/cornerstone/static/css/main.ffbfdec3.chunk.css"
+  },
+  {
+    "revision": "6878b74c1bdf65f15c53",
+    "url": "/cornerstone/static/js/2.652b8c6a.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/cornerstone/static/js/2.652b8c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556d9d25f6d803f7d899",
+    "url": "/cornerstone/static/js/main.2f3ffee7.chunk.js"
+  },
+  {
+    "revision": "b17aec86929838d9fd46",
+    "url": "/cornerstone/static/js/runtime-main.2fff7642.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cornerstone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cornerstone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cornerstone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cornerstone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cornerstone/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
